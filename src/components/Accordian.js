d0df468815fb8f0d3746ef07f69007bcb0d686a9
ankitt01 <ankitrawat0102@gmail.com>
2021-05-24 00:00:27 +0530

    Helper Function

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Accordian = ({items}) => {
+
+
+    const onTitleClick = (index) => {
+        console.log("Title Clicked" , index);
+    }
+
     const renderedItems = items.map((item, index) => {
         return (
             <React.Fragment key={item.title}>
                 <div className="title active"
-                    onClick={() => console.log("Title Clicked" , index)} >
+                    onClick={() => onTitleClick(index)} >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
