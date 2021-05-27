cd1ff6c2b2961361507dcb3c01035c25bc353174
ankitt01 <ankitrawat0102@gmail.com>
2021-05-27 15:13:45 +0530

    Building lists

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,9 +5,10 @@ const Search = () => {
     const [term,setTerm] = useState('Programming');
     const [results, setResults] = useState([]);
 
+
     useEffect(() => {
         const search = async () => {
-            const {data} = await axios.get('https://encodeURI.wikipedia.org/w/api.php', {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
@@ -16,12 +17,25 @@ const Search = () => {
                     srsearch: term,
                 }
             })
-
             setResults(data.query.search);
-
         }
-        
+        search();
     }, [term])
+
+    const renderedResults = results.map((result) => {
+        
+        return (
+            <div key={result.pageid} className="item">
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    {result.snippet}
+                </div>
+            </div>
+        )
+    })
+    
     return (
         <div>
             <div className="ui form container">
@@ -30,6 +44,11 @@ const Search = () => {
                     <input type="text" className="input" value={term} onChange = {e => setTerm(e.target.value)} />
                 </div>
             </div>
+
+            <div className="ui celled list">
+                {renderedResults}
+            </div>
+
         </div>
     )
 
