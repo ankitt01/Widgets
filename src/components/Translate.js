49b085493fcf79854e276b896c815d74d58d0ca1
ankitt01 <ankitrawat0102@gmail.com>
2021-05-31 22:13:12 +0530

    Convert component

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Dropdown from './Dropdown';
-
+import Convert from './Convert'
+//AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
 const options = [
     {
         label: 'Africans',
@@ -19,9 +20,20 @@ const options = [
 const Translate = () => {
 
     const [language, setLanguage] = useState(options[0]);
+    const [text,setText] = useState('');
     return (
         <div>
+            <div className="ui form container">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+                </div>
+
             <Dropdown label="Select a Language" options={options} selected={language} onSelectedChange={setLanguage}/>
+            <hr />
+            <h3 className="ui header">Output</h3>
+            <Convert text={text} language={language} />
+            </div>
         </div>
     )
 }
