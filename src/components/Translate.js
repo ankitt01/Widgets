79724003950fa54377fd54043f650b065fb2f6a8
ankitt01 <ankitrawat0102@gmail.com>
2021-05-31 21:49:55 +0530

    re usable

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -21,7 +21,7 @@ const Translate = () => {
     const [language, setLanguage] = useState(options[0]);
     return (
         <div>
-            <Dropdown options={options} selected={language} onSelectedChange={setLanguage}/>
+            <Dropdown label="Select a Language" options={options} selected={language} onSelectedChange={setLanguage}/>
         </div>
     )
 }
